325ff2acf069a13dc98e4b9cd9b709e1f1b8de18
Leigh Halliday <leighhalliday@gmail.com>
2015-06-10 07:23:08 -0400

    Adding pg for production

--- config/environment.rb
+++ config/environment.rb
@@ -27,5 +27,12 @@ end
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
 
+# Load all models from app/models, using autoload instead of require
+# See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
+Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+  filename = File.basename(model_file).gsub('.rb', '')
+  autoload ActiveSupport::Inflector.camelize(filename), model_file
+end
+
 # Load the routes / actions
 require APP_ROOT.join('app', 'actions')
