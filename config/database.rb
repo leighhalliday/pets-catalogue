6ec9e4d67034c45f1e7c92e7ef3f05786a6a347b
Leigh Halliday <leighhalliday@gmail.com>
2015-06-10 07:26:27 -0400

    Database for prod

--- config/database.rb
+++ config/database.rb
@@ -1,12 +1,17 @@
 configure do
+
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+    set :database, {
+      adapter: "sqlite3",
+      database: "db/db.sqlite3"
+    }
   end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
+  if Sinatra::Application.production?
+    ActiveRecord::Base.establish_connection ENV['DATABASE_URL']
+  end
 
 end
